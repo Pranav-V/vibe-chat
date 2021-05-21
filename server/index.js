17fc64925cc6063751833b0ef7d84c43b46e341e
Pranav <pvenkatesh3141@gmail.com>
2021-05-21 15:45:13 -0500

    d

--- server/index.js
+++ server/index.js
@@ -4,10 +4,14 @@ const http = require('http')
 
 const PORT = process.env.PORT || 5000
 
+const router = require('./router')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+app.use(router)
+
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
+
